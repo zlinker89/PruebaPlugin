928bdb1cf772a215897095302c999a9a3ac9d2ad
Franklin Ospino <desarrollo@expertosip.com>
2021-04-19 11:56:48 -0500

    Prueba de plugin en app android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import 'capacitor-plugin-myplugin';
 
+const { Alert } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  Mensaje : string = "hola mundo";
+  op: any = {
+    value1: 0,
+    value2: 0
+  };
+  resultado: number = 0;
   constructor() {}
 
+  showMensaje = () => {
+    Alert.echo({value: this.Mensaje});
+  }
+
+  sumar = async () => {
+    try {
+      const result = await (Alert.sumar(this.op));
+      console.log("Encaso de error", result); 
+      this.resultado = result.suma;
+    } catch (error) {
+      alert(JSON.stringify(error))
+    }
+  }
+  
 }
